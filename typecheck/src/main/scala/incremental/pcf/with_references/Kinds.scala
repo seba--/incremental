fd9207b01e43510c10176b921a14b452f27bb788
Sebastian Erdweg <seba.github@gmail.com>
2015-04-07 18:53:13 +0200

    syntax checks for refs

--- typecheck/src/main/scala/incremental/pcf/with_references/Kinds.scala
+++ typecheck/src/main/scala/incremental/pcf/with_references/Kinds.scala
@@ -1,11 +1,11 @@
 package incremental.pcf.with_references
 
-import incremental.ExpKind
+import incremental.{Syntax, ExpKind}
 
 /**
  * Created by seba on 15/11/14.
  */
-case object Ref extends ExpKind
-case object Deref extends ExpKind
-case object Assign extends ExpKind
-case object Seq extends ExpKind
+case object Ref extends ExpKind(Syntax(1))
+case object Deref extends ExpKind(Syntax(1))
+case object Assign extends ExpKind(Syntax(2))
+case object Seq extends ExpKind(Syntax(2))
