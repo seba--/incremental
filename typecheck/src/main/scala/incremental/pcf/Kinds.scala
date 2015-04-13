4d8bf2abc320ae8fd3c94f62dc3fe712c3094ad4
Edlira Kuci <edlirakuci@hotmail.com>
2015-04-13 09:32:44 +0200

    resolve merge

--- typecheck/src/main/scala/incremental/pcf/Kinds.scala
+++ typecheck/src/main/scala/incremental/pcf/Kinds.scala
@@ -9,17 +9,6 @@ import incremental.Typ
  * Created by seba on 13/11/14.
  */
 
-<<<<<<< HEAD
-case object Num extends ExpKind(0)
-case object Add extends ExpKind(2)
-case object Mul extends ExpKind(2)
-case object Var extends ExpKind(0)
-case object Abs extends ExpKind(1)
-case object App extends ExpKind(2)
-case object If0 extends ExpKind(3)
-case object Fix extends ExpKind(1)
-case object Field extends ExpKind(2)
-=======
 abstract class Exp(syntaxcheck: SyntaxChecking.SyntaxCheck) extends NodeKind(syntaxcheck)
 object Exp {
   val cExp = classOf[Exp]
@@ -34,4 +23,4 @@ case object Abs extends Exp(simple(Seq(classOf[Symbol]), cExp) orElse simple(Seq
 case object App extends Exp(simple(cExp, cExp))
 case object If0 extends Exp(simple(cExp, cExp, cExp))
 case object Fix extends Exp(simple(cExp))
->>>>>>> origin/master
+
