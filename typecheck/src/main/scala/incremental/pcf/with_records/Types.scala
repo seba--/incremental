8b43430ed365dcd7fbad68f5e6d957f28b504019
Sebastian Erdweg <seba.github@gmail.com>
2015-04-30 10:02:13 +0200

    with-records fixes

--- typecheck/src/main/scala/incremental/pcf/with_records/Types.scala
+++ typecheck/src/main/scala/incremental/pcf/with_records/Types.scala
@@ -1,9 +1,7 @@
 package incremental.pcf.with_records
 
-import incremental.{EqConstraint, Type}
-import incremental.ConstraintOps._
-import incremental.Type.Companion.TSubst
-import incremental.pcf.UVar
+import constraints.equality._
+import constraints.equality.Type.Companion.TSubst
 
 /**
  * Created by seba on 15/11/14.
@@ -13,14 +11,14 @@ case class TRecord(fields: Map[Symbol, Type]) extends Type {
   def occurs(x: Symbol) = fields.exists(_._2.occurs(x))
   def normalize = TRecord(fields.mapValues(_.normalize))
   def subst(s: TSubst) = TRecord(fields.mapValues(_.subst(s)))
-  def unify(other: Type, s: TSubst) = other match {
+  def unify[CS <: ConstraintSystem[CS]](other: Type, s: TSubst)(implicit csf: ConstraintSystemFactory[CS]) = other match {
     case TRecord(fields2) if fields.keys == fields2.keys => {
-      var sol = emptySol
+      var sol = csf.emptySolution
       for (k <- fields.keys)
-        sol = sol ++ fields(k).unify(fields2(k), s)
+        sol = sol mergeSubsystem fields(k).unify(fields2(k), s)
       sol
     }
     case UVar(_) => other.unify(this, s)
-    case _ => never(EqConstraint(this, other))
+    case _ => csf.never(EqConstraint(this, other))
   }
 }
