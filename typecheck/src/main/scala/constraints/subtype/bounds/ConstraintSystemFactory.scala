ba4fddd9003a6fba6fa7d946d58f801e806ce863
Sebastian Erdweg <seba.github@gmail.com>
2015-04-30 16:42:37 +0200

    solve subtyping end

--- typecheck/src/main/scala/constraints/subtype/bounds/ConstraintSystemFactory.scala
+++ typecheck/src/main/scala/constraints/subtype/bounds/ConstraintSystemFactory.scala
@@ -6,6 +6,4 @@ import constraints.subtype.Type.Companion._
 
 abstract class ConstraintSystemFactory[CS <: subtype.ConstraintSystem[CS]] extends subtype.ConstraintSystemFactory[CS] {
   val defaultBounds = Map[Symbol, (LBound, UBound)]().withDefaultValue((LBound(Set(), None), UBound(Set(), None)))
-
-  def system(substitution: TSubst, bounds: Map[Symbol, (LBound, UBound)], never: Seq[Constraint]): CS
 }
