5549816c174be36bc9c0c6e0915eb5a4fcb0d14a
Edlira Kuci <edlirakuci@hotmail.com>
2015-05-19 16:02:03 +0200

    test correctness

--- typecheck/src/test/scala/incremental/FJava/TestCorrectness.scala
+++ typecheck/src/test/scala/incremental/FJava/TestCorrectness.scala
@@ -4,6 +4,7 @@ import constraints.CVar
 import constraints.equality.impl._
 import constraints.equality._
 import incremental.Node._
+import incremental.systemf.UVar
 import incremental.{Node_, Util}
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
@@ -14,8 +15,17 @@ class TestCorrectness[CS <: ConstraintSystem[CS]](classdesc: String, checkerFact
   val checker: BUChecker[CS] = checkerFactory.makeChecker
 
   override def afterEach: Unit = checker.localState.printStatistics()
+  def typecheckTest(desc: String, e: =>Node)(expected: Type): Unit =
+    test (s"$classdesc: Type check $desc") {
+      val actual = checker.typecheck(e)
+      assert(actual.isLeft, s"Expected $expected but got $actual")
+      val sol = SolveContinuously.state.withValue(checker.csFactory.state.value) {
+        expected.unify(actual.left.get, SolveContinuously.freshConstraintSystem).tryFinalize
+      }
+      assert(sol.isSolved, s"Expected $expected but got ${actual.left.get}. Match failed with ${sol.unsolved}")
+    }
 
-  def typecheckTest(desc: String, e: =>Node_[checker.Result])(expected: Type): Unit =
+  def typecheckTestFJ(desc: String, e: =>Node_[checker.Result])(expected: Type): Unit =
     test (s"$classdesc: Type check $desc") {
       val actual = checker.typecheck(e.withType[Any])
       assert(actual.isLeft, s"Expected $expected but got $actual")
@@ -33,15 +43,15 @@ class TestCorrectness[CS <: ConstraintSystem[CS]](classdesc: String, checkerFact
     }
 
 
-  typecheckTestError("x", Var('x))
-  typecheckTestError("e0.f : U ", Field('f,Var('e0)))
+  typecheckTestFJ("x", Var('x))(UVar(CVar('x$1)))
+  typecheckTestError("e0.f : U ", Fields('f,Var('e0)))
   typecheckTestError("new C(x):C", New(CName('c),Var('x)))
   typecheckTestError("e0.m()", Invk(Seq('m), Seq(Var('e0))))
   typecheckTestError("e0.m(e) : U", Invk(Seq('m), Seq(Var('e0), Var('e))))
   typecheckTestError("Pair.m(e1, e2)", Invk(Seq('m), Seq(Var('pair),Var('e1), Var('e2))))
   typecheckTestError("(C) e0 :C", UCast(CName('c),Var('e)))
   typecheckTestError("y", Var('x))
-  typecheckTestError(" (new Pair(first)).first : U", Field('first,New(CName('Pair),Var('first))))
+  typecheckTestError(" (new Pair(first)).first : U", Fields('first,New(CName('Pair),Var('first))))
   typecheckTestError("new Pair(first) : Pair", New(CName('Pair),Var('first)))
   typecheckTestError("new Pair(snd): Pair", New(CName('Pair),Var('object)))
   typecheckTestError("Pair.setfst(first) : U ", Invk(Seq('setfst),Seq(New(CName('Pair),Var('first)), Var('first))))
