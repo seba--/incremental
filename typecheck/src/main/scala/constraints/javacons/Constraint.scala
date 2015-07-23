99d041826ff149e65801b5ca7179b93eb3054f45
gnush <gnush@hushmail.com>
2015-07-23 14:42:09 +0200

    added widening constraints

--- typecheck/src/main/scala/constraints/javacons/Constraint.scala
+++ typecheck/src/main/scala/constraints/javacons/Constraint.scala
@@ -11,6 +11,22 @@ trait Constraint extends constraints.Constraint[Gen, Constraint]{
 
 }
 
-case class EqConstraint(expected: Type, actual: Type) extends Constraint {
-  def subst(s: CSubst) = EqConstraint(expected.subst(s), actual.subst(s))
+case class Equality(expected: Type, actual: Type) extends Constraint {
+  def subst(s: CSubst) = Equality(expected.subst(s), actual.subst(s))
+}
+
+case class PrimitiveWidening(actual: Type, expected: Type) extends Constraint {
+  def subst(s: CSubst) = PrimitiveWidening(actual.subst(s), expected.subst(s))
+}
+
+case class PrimitiveWideningString(actual: Type, expected: Type) extends Constraint {
+  def subst(s: CSubst) = PrimitiveWideningString(actual.subst(s), expected.subst(s))
+}
+
+case class PrimitiveWideningEq(res: Type, actual: Type, expected: Type) extends Constraint {
+  def subst(s: CSubst) = PrimitiveWideningEq(res.subst(s), actual.subst(s), expected.subst(s))
+}
+
+case class SortOf(actual: Type, expected: Seq[Type]) extends Constraint {
+  def subst(s: CSubst) = SortOf(actual.subst(s), expected.map(_.subst(s)))
 }
