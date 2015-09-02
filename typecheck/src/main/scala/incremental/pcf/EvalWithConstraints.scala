1c1541133d3be9c8c2070cf83a10ee65681b9e13
Sebastian Erdweg <seba.github@gmail.com>
2015-09-02 00:20:52 -0700

    structural representation of constraint sets

--- typecheck/src/main/scala/incremental/pcf/EvalWithConstraints.scala
+++ typecheck/src/main/scala/incremental/pcf/EvalWithConstraints.scala
@@ -1,5 +1,7 @@
 package incremental.pcf
 
+import java.util.Objects
+
 import constraints.equality.Gen
 import incremental.Node.Node
 import incremental.Node_
@@ -16,8 +18,8 @@ object EvalWithConstraints {
   case class VNum(n: Integer) extends Val {
     override def toString = n.toString
   }
-  case class VFun(vx: UVar, body: Val) extends Val { // dynamically scoped
-    override def toString = s"($vx => $body)"
+  case class VFun(vx: UVar, body: Val, c: Cons) extends Val { // dynamically scoped
+    override def toString = s"($vx => $body | ${c.toSeq.mkString(", ")})"
   }
   case class UVar(x: Symbol, private var box: Box[Val] = new Box(null)) extends Val {
     override def get = box.v
@@ -29,12 +31,21 @@ object EvalWithConstraints {
       else
         box.v = v
     }
+
     def isSet = box.v != null
+
     override def toString =
       if (isSet)
         get.toString
       else
         "$" + x.name
+
+    override def hashCode() = 31 * x.hashCode + Objects.hashCode(box.v)
+
+    override def equals(obj: scala.Any) = obj.isInstanceOf[UVar] && {
+      val o = obj.asInstanceOf[UVar]
+      x == o.x && Objects.equals(box.v, o.box.v) || box == o.box
+    }
   }
 
   val gen = new Gen
@@ -73,13 +84,22 @@ object EvalWithConstraints {
       case (VNum(n1), VNum(n2)) => solved(vres.set(VNum(n1 * n2)))
       case _ => never
     }
+    override def toString = s"$vres = $v1 * $v2"
   }
   case class VApp(vres: UVar)(v1: Val, v2: Val) extends VConstraint {
     override def solve = v1.get match {
       case null => notyet
-      case VFun(vx, body) => solved {vx.set(v2); vres.set(body)}
+      case VFun(vx, body, c) =>
+        vx.set(v2)
+        vres.set(body)
+        val rest = c.fixsolve
+        if (rest == CEmpty)
+          solved{}
+        else
+          notyet
       case _ => never
     }
+    override def toString = s"$vres = $v1($v2)"
   }
   case class VIf0(vres: UVar)(v1: Val, v2: Val, v3: Val) extends VConstraint {
     override def solve = v1.get match {
@@ -91,9 +111,55 @@ object EvalWithConstraints {
           solved(vres.set(v3))
       case _ => never
     }
+    override def toString = s"$vres = if0($v1, $v2, $v3)"
   }
 
-  type Cons = Seq[VConstraint]
+  trait Cons {
+    val size: Int
+    def isEmpty = size == 0
+    def ++(cs: Cons): Cons
+    def :+(c: VConstraint): Cons = CInsert(c, this)
+    def toSeq: Seq[VConstraint]
+    def solve: Cons
+    def fixsolve: Cons = {
+      var rest = this
+      var oldsize = rest.size
+      var newsize = oldsize
+      do {
+        oldsize = newsize
+        rest = rest.solve
+        newsize = rest.size
+      } while (newsize < oldsize)
+      rest
+    }
+  }
+  case object CEmpty extends Cons {
+    val size = 0
+    def ++(cs: Cons) = cs
+    override def toSeq = Seq()
+    def solve = CEmpty
+  }
+  case class CInsert(c: VConstraint, cs: Cons) extends Cons {
+    val size = cs.size + 1
+    override def ++(cs: Cons) = CConcat(Seq(this, cs))
+    override def toSeq = c +: cs.toSeq
+    override def solve =
+      if (c.solve)
+        cs.solve
+      else
+        cs.solve :+ c
+  }
+  case class CConcat(css: Seq[Cons], _size: Int = -1) extends Cons {
+    val size =
+      if (_size >= 0)
+        _size
+      else
+        css.foldLeft[Int](0)((n,cs) => n + cs.size)
+    override def ++(cs: Cons) = CConcat(css :+ cs, size + cs.size)
+    override def toSeq = css.foldLeft[Seq[VConstraint]](Seq())((seq,cs) => seq ++ cs.toSeq)
+    override def solve = css.foldLeft[Cons](CEmpty)((res,cs) => res ++ cs.solve)
+  }
+
   type Reqs = Map[Symbol, UVar]
   type StepResult = (Val, Reqs, Cons)
 
@@ -107,24 +173,16 @@ object EvalWithConstraints {
 
     val (v, r, c) = root.typ
 
-    var oldsize = c.size
-    var newsize = oldsize
-    var unsolved = c
-    do {
-      oldsize = newsize
-      unsolved = unsolved.filter(!_.solve)
-      newsize = unsolved.size
-    } while (newsize < oldsize)
-
-    if (unsolved.isEmpty && r.isEmpty)
+    val rest = c.fixsolve
+    if (rest.isEmpty && r.isEmpty)
       Left(v.get)
     else
-      Right(s"Unsolved requirements=$r, value=$v, unsolved constraints=$unsolved")
+      Right(s"Unsolved requirements=$r, value=$v, unsolved constraints=${rest.toSeq}")
   }
 
   def evalStep(e: Node_[StepResult]): StepResult = e.kind match {
     case Num =>
-      (VNum(e.lits(0).asInstanceOf[Integer]), Map(), Seq())
+      (VNum(e.lits(0).asInstanceOf[Integer]), Map(), CEmpty)
 
     case Add =>
       val (v1, r1, c1) = e.kids(0).typ
@@ -143,14 +201,15 @@ object EvalWithConstraints {
     case Var =>
       val x = e.lits(0).asInstanceOf[Symbol]
       val v = freshUVar()
-      (v, Map(x -> v), Seq())
+      (v, Map(x -> v), CEmpty)
 
     case Abs =>
       val x = e.lits(0).asInstanceOf[Symbol]
       val (v, r, c) = e.kids(0).typ
+      val crest = c.solve
       r.get(x) match {
-        case None => (VFun(freshUVar(), v), r, c)
-        case Some(vx) => (VFun(vx, v), r - x, c)
+        case None => (VFun(freshUVar(), v, crest), r, CEmpty)
+        case Some(vx) => (VFun(vx, v, crest), r - x, CEmpty)
       }
 
     case App =>
@@ -166,7 +225,7 @@ object EvalWithConstraints {
       val (v3, r3, c3) = e.kids(2).typ
       val v = freshUVar()
       val (mc, mr) = mergeReqMaps(r1, r2, r3)
-      (v, mr, c1 ++ c2 ++ c3++ mc :+ VIf0(v)(v1, v2, v3))
+      (v, mr, c1 ++ c2 ++ c3 ++ mc :+ VIf0(v)(v1, v2, v3))
   }
 
 
@@ -178,14 +237,14 @@ object EvalWithConstraints {
 
 
 
-  private val init: (Seq[VConstraint], Reqs) = (Seq(), Map())
+  private val init: (Cons, Reqs) = (CEmpty, Map())
 
-  def mergeReqMaps(req: Reqs, reqs: Reqs*): (Seq[VConstraint], Reqs) = mergeReqMaps(req +: reqs)
+  def mergeReqMaps(req: Reqs, reqs: Reqs*): (Cons, Reqs) = mergeReqMaps(req +: reqs)
 
-  def mergeReqMaps(reqs: Seq[Reqs]): (Seq[VConstraint], Reqs) =
-    reqs.foldLeft[(Seq[VConstraint], Reqs)](init)(_mergeReqMaps)
+  def mergeReqMaps(reqs: Seq[Reqs]): (Cons, Reqs) =
+    reqs.foldLeft[(Cons, Reqs)](init)(_mergeReqMaps)
 
-  private def _mergeReqMaps(was: (Seq[VConstraint], Reqs), newReqs: Reqs) = {
+  private def _mergeReqMaps(was: (Cons, Reqs), newReqs: Reqs) = {
     val wasReqs = was._2
     var mcons = was._1
     var mreqs = wasReqs
@@ -193,7 +252,7 @@ object EvalWithConstraints {
       wasReqs.get(x) match {
         case None => mreqs += x -> r2
         case Some(r1) =>
-          mcons = VEq(r1)(r2) +: mcons
+          mcons = mcons :+ VEq(r1)(r2)
       }
     (mcons, mreqs)
   }
--- typecheck/src/test/scala/incremental/pcf/TestEvalWithConstraints.scala
+++ typecheck/src/test/scala/incremental/pcf/TestEvalWithConstraints.scala
@@ -4,30 +4,62 @@ import incremental.Node._
 import incremental.pcf.EvalWithConstraints._
 import org.scalatest.FunSuite
 
+
 class TestEvalWithConstraints extends FunSuite {
+  import incremental.pcf.EvalWithConstraints.UVar
 
   def testEval(desc: String, e: => Node)(v: Val) =
     test(desc) {
       val res = eval(e)
       if (res.isLeft) {
-        val resVal = res.left.get
-        assert(v == resVal)
+        val r = res.left.get
+        assert(v == r)
       }
       else
         fail(s"Evaluation failed with ${res.right.get}, but expected $v")
     }
+  def testEvalCheck(desc: String, e: => Node)(check: Val => Option[String]) =
+    test(desc) {
+      val res = eval(e)
+      if (res.isLeft) {
+        val resVal = res.left.get
+        check(resVal) match {
+          case None =>
+          case Some(s) => fail(s)
+        }
+      }
+      else
+        fail(s"Evaluation failed with ${res.right.get}")
+    }
 
   testEval("2+3", Add(Num(2), Num(3)))(VNum(5))
 
-  testEval("x => x+3", Abs('x, Add(Var('x), Num(3))))(VNum(5))
+  testEval("x => x+3", Abs('x, Add(Var('x), Num(3)))){
+    val vx = UVar('v1)
+    val vres = UVar('v2)
+    val add = VAdd(vres)(vx, VNum(3))
+    VFun(vx, vres, CEmpty :+ add)
+  }
 
   testEval("(x => x+3) 2", App(Abs('x, Add(Var('x), Num(3))), Num(2)))(VNum(5))
 
-  testEval("x => (x+1)+2", Abs('x, Add(Add(Var('x), Num(1)), Num(2))))(VNum(5))
+  testEval("x => (x+1)+2", Abs('x, Add(Add(Var('x), Num(1)), Num(2)))){
+    val vx = UVar('v6)
+    val vres = UVar('v8)
+    val vres1 = UVar('v7)
+    val add2 = VAdd(vres)(vres1, VNum(2))
+    val add1 = VAdd(vres1)(vx, VNum(1))
+    VFun(vx, vres, ((CEmpty :+ add1) ++ CEmpty ++ CEmpty) :+ add2)
+  }
 
   testEval("(x => (x+1)+2) 2", App(Abs('x, Add(Add(Var('x), Num(1)), Num(2))), Num(2)))(VNum(5))
 
-  testEval("x => x+(1+2)", Abs('x, Add(Var('x), Add(Num(1), Num(2)))))(VNum(5))
+  testEval("x => x+(1+2)", Abs('x, Add(Var('x), Add(Num(1), Num(2))))){
+    val vx = UVar('v13)
+    val vres = UVar('v15)
+    val add = VAdd(vres)(vx, VNum(3))
+    VFun(vx, vres, CEmpty :+ add)
+  }
 
   testEval("(x => x+(1+2)) 2", App(Abs('x, Add(Var('x), Add(Num(1), Num(2)))), Num(2)))(VNum(5))
 }
