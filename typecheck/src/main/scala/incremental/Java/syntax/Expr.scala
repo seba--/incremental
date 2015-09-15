82a922cfe983d4b8364c280e885b278de0635cd0
gnush <gnush@hushmail.com>
2015-09-15 16:35:37 +0200

    implemented chech for arithmetic operators

--- typecheck/src/main/scala/incremental/Java/syntax/Expr.scala
+++ typecheck/src/main/scala/incremental/Java/syntax/Expr.scala
@@ -221,16 +221,157 @@ case object Minus extends Expr(simple(cExpr, cExpr) orElse (simple(cExpr))){
     }
   }
 }
-case object Mul extends Expr(simple(cExpr, cExpr))
-case object Div extends Expr(simple(cExpr, cExpr))
-case object Remain extends Expr(simple(cExpr, cExpr))
-case object PreIncr extends Expr(simple(cExpr))
-case object PostIncr extends Expr(simple(cExpr))
-case object PreDecr extends Expr(simple(cExpr))
-case object PostDecr extends Expr(simple(cExpr))
-case object LeftShift extends Expr(simple(cExpr, cExpr))
-case object RightShift extends Expr(simple(cExpr, cExpr))
-case object URightShift extends Expr(simple(cExpr, cExpr))
+case object Mul extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(1).typ
+
+    val X = freshUVar()
+
+    val widen = PrimitiveWidening(t1, t2)
+    val widenEq = PrimitiveWideningEq(X, t1, t2)
+    val oneOfCons = Seq(OneOf(t1, numTypes)
+                      , OneOf(t2, numTypes)
+                      , OneOf( X, numericOpsTypes))
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons ++ oneOfCons :+ widen :+ widenEq
+
+    (ExprType(X), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
+case object Div extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(1).typ
+
+    val X = freshUVar()
+
+    val widen = PrimitiveWidening(t1, t2)
+    val widenEq = PrimitiveWideningEq(X, t1, t2)
+    val oneOfCons = Seq(OneOf(t1, numTypes)
+                      , OneOf(t2, numTypes)
+                      , OneOf( X, numericOpsTypes))
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons ++ oneOfCons :+ widen :+ widenEq
+
+    (ExprType(X), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
+case object Remain extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(1).typ
+
+    val X = freshUVar()
+
+    val widen = PrimitiveWidening(t1, t2)
+    val widenEq = PrimitiveWideningEq(X, t1, t2)
+    val oneOfCons = Seq(OneOf(t1, numTypes)
+                      , OneOf(t2, numTypes)
+                      , OneOf( X, numericOpsTypes))
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons ++ oneOfCons :+ widen :+ widenEq
+
+    (ExprType(X), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
+case object PreIncr extends Expr(simple(cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t), vReqs, cReqs, cons) = kids(0).typ
+
+    val X = freshUVar()
+
+    val eq = Equality(t, X)
+    val tInNum = OneOf(t, numTypes)
+    val xInNumOps = OneOf(X, numericOpsTypes)
+
+    (ExprType(X), vReqs, cReqs, cons :+ eq :+ tInNum :+ xInNumOps)
+  }
+}
+case object PostIncr extends Expr(simple(cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t), vReqs, cReqs, cons) = kids(0).typ
+
+    val X = freshUVar()
+
+    val eq = Equality(t, X)
+    val tInNum = OneOf(t, numTypes)
+    val xInNumOps = OneOf(X, numericOpsTypes)
+
+    (ExprType(X), vReqs, cReqs, cons :+ eq :+ tInNum :+ xInNumOps)
+  }
+}
+case object PreDecr extends Expr(simple(cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t), vReqs, cReqs, cons) = kids(0).typ
+
+    val X = freshUVar()
+
+    val eq = Equality(t, X)
+    val tInNum = OneOf(t, numTypes)
+    val xInNumOps = OneOf(X, numericOpsTypes)
+
+    (ExprType(X), vReqs, cReqs, cons :+ eq :+ tInNum :+ xInNumOps)
+  }
+}
+case object PostDecr extends Expr(simple(cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t), vReqs, cReqs, cons) = kids(0).typ
+
+    val X = freshUVar()
+
+    val eq = Equality(t, X)
+    val tInNum = OneOf(t, numTypes)
+    val xInNumOps = OneOf(X, numericOpsTypes)
+
+    (ExprType(X), vReqs, cReqs, cons :+ eq :+ tInNum :+ xInNumOps)
+  }
+}
+case object LeftShift extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(0).typ
+
+    val t1InIntegral = OneOf(t1, integralTypes)
+    val t2InIntegral = OneOf(t2, integralTypes)
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons :+ t1InIntegral :+ t2InIntegral
+
+    (ExprType(t1), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
+case object RightShift extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(0).typ
+
+    val t1InIntegral = OneOf(t1, integralTypes)
+    val t2InIntegral = OneOf(t2, integralTypes)
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons :+ t1InIntegral :+ t2InIntegral
+
+    (ExprType(t1), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
+case object URightShift extends Expr(simple(cExpr, cExpr)){
+  def check(lits: Seq[Any], kids: Seq[Kid]): StepResult = {
+    val (ExprType(t1), vReqs1, cReqs1, cons1) = kids(0).typ
+    val (ExprType(t2), vReqs2, cReqs2, cons2) = kids(0).typ
+
+    val t1InIntegral = OneOf(t1, integralTypes)
+    val t2InIntegral = OneOf(t2, integralTypes)
+
+    val (mCons, mReqs) = mergeVReqs(vReqs1, vReqs2)
+    val cons = cons1 ++ cons2 ++ mCons :+ t1InIntegral :+ t2InIntegral
+
+    (ExprType(t1), mReqs, mergeCReqs(cReqs1, cReqs2), cons)
+  }
+}
 
 // Bitwise Operators
 case object Complement extends Expr(simple(cExpr))
