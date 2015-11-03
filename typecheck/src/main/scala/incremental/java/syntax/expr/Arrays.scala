28896935564c554afab65d263c3142515c93b784
gnush <gnush@hushmail.com>
2015-11-03 18:57:23 +0100

    added context to signature of check in Arrays and Assignments

--- typecheck/src/main/scala/incremental/java/syntax/expr/Arrays.scala
+++ typecheck/src/main/scala/incremental/java/syntax/expr/Arrays.scala
@@ -4,7 +4,7 @@ import constraints.javacons._
 import incremental.Node._
 import incremental.java.JavaCheck._
 import incremental.java.syntax._
-import incremental.{NodeKind, SyntaxChecking}
+import incremental.{Context, NodeKind, SyntaxChecking}
 import incremental.java.syntax.expr.Expr._
 
 /**
@@ -13,19 +13,19 @@ import incremental.java.syntax.expr.Expr._
 
 // Array Access
 case object ArrayAccess extends Expr(simple(cExpr, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 
 // Array Creation
 case object NewArray extends Expr(_ => ArrayCreationSyntax){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 
 trait ArrayBaseType{} // extends PrimType with TypeName{}
 case class UnboundWld(t: TypeName) extends ArrayBaseType
 
 abstract class Dimension(syntaxcheck: SyntaxChecking.SyntaxCheck) extends NodeKind[Constraint, Result](syntaxcheck){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object Dim extends Dimension(simple())
 case object DimExpr extends Dimension(simple(cExpr))
--- typecheck/src/main/scala/incremental/java/syntax/expr/Assignments.scala
+++ typecheck/src/main/scala/incremental/java/syntax/expr/Assignments.scala
@@ -1,6 +1,7 @@
 package incremental.java.syntax.expr
 
 import constraints.javacons._
+import incremental.Context
 import incremental.Node._
 import incremental.SyntaxChecking._
 import incremental.java.JavaCheck._
@@ -13,38 +14,38 @@ import incremental.java.syntax.expr.Expr._
 
 // Assignment Operators
 case object Assign extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignMul extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignDiv extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignRemain extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignPlus extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignMinus extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignLeftShift extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignRightShift extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignURightShift extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignAnd extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignExcOr extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
 case object AssignOr extends Expr(simple(ExprName.getClass, cExpr) orElse simple(classOf[FieldAccess], cExpr) orElse simple(ArrayAccess.getClass, cExpr)){
-  def check(lits: Seq[Any], kids: Seq[Kid]): Result = ???
+  def check(lits: Seq[Any], kids: Seq[Kid], context: Context[Constraint]): Result = ???
 }
