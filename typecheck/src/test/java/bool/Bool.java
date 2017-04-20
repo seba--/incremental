69a2791054db9364fb2bf64b500314055970e408
Sebastian Erdweg <seba.github@gmail.com>
2017-04-20 17:48:12 +0200

    fix merge bug

--- typecheck/src/test/java/bool/Bool.java
+++ typecheck/src/test/java/bool/Bool.java
@@ -4,10 +4,6 @@ import block.Block;
 
 public class Bool {
     public Bool() {}
-    public abstract Bool not();
-    public abstract Bool and(Bool that);
-    public abstract Bool or(Bool that);
-    public abstract Object ifTrue(Block thn, Block els);
     public /*abstract*/ Bool not() { return new False(); }
     public /*abstract*/ Bool and(Bool that) { return new False(); }
     public /*abstract*/ Bool or(Bool that) { return new False(); }
