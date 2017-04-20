f7197f3f0d2b8c18146bb29477c01040c94fad6b
Edlira Kuci <edlirakuci@hotmail.com>
2017-04-20 20:58:28 +0200

    clean master

--- typecheck/src/test/java/bool/Bool.java
+++ typecheck/src/test/java/bool/Bool.java
@@ -4,6 +4,13 @@ import block.Block;
 
 public class Bool {
     public Bool() {}
+<<<<<<< HEAD
+    public abstract Bool not();
+    public abstract Bool and(Bool that);
+    public abstract Bool or(Bool that);
+    public abstract Object ifTrue(Block thn, Block els);
+=======
+>>>>>>> 266f569b25f1194043c8296dcd91c70fd2c47325
     public /*abstract*/ Bool not() { return new False(); }
     public /*abstract*/ Bool and(Bool that) { return new False(); }
     public /*abstract*/ Bool or(Bool that) { return new False(); }
