913b11afd9007511f77b4cd273c235acfbb0be4b
Sebastian Erdweg <seba.github@gmail.com>
2017-04-20 15:48:35 +0200

    make superclasses non-abstract as required by FJ

--- typecheck/src/test/java/nat/Nat.java
+++ typecheck/src/test/java/nat/Nat.java
@@ -2,17 +2,17 @@ package nat;
 
 import bool.*;
 
-public abstract class Nat {
+public class Nat {
     public Nat() {}
-    public abstract Bool isZero();
+    public /*abstract*/ Bool isZero() { return new False(); }
     public Bool isNonZero() {
         return isZero().not();
     }
-    public abstract Nat pred();
-    public abstract Bool lessThan(Nat that);
-    public abstract Bool equal(Nat that);
+    public /*abstract*/ Nat pred() { return new Zero(); }
+    public /*abstract*/ Bool lessThan(Nat that) { return new False(); }
+    public /*abstract*/ Bool equal(Nat that) { return new False(); }
     public Bool greaterThan(Nat that) {
         return that.lessThan(this);
     }
-    public abstract Nat plus(Nat that);
+    public /*abstract*/ Nat plus(Nat that) { return new Zero(); }
 }
