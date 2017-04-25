64ec7a27ab9e05855c3f2dff4c99cbb850232b09
Sebastian Erdweg <seba.github@gmail.com>
2017-04-26 00:05:10 +0200

    provide parsed program as lazy val

--- typecheck/src/main/scala/util/parse/Parse.scala
+++ typecheck/src/main/scala/util/parse/Parse.scala
@@ -9,15 +9,19 @@ import incremental.fjava.ProgramM
   * Created by oliver on 20.04.17.
   */
 object Parse extends App {
-  val dir = "src"/"main"/"java"
-  val nodes = dir.glob("**/*.java").filter(f => !f.nameWithoutExtension.endsWith("Test") && f.nameWithoutExtension != "Block").flatMap { f =>
-    println(f.path)
-    val nodes = JavaToFJ(JavaParser.parse(f.toJava))
-    val fj = f.parent/ s"${f.nameWithoutExtension}.fj"
-    fj.write(nodes.mkString("\n\n"))
-    nodes
+
+  lazy val prog: Node = {
+    val dir = "src" / "main" / "java"
+    val nodes = dir.glob("**/*.java").filter(f => !f.nameWithoutExtension.endsWith("Test") && f.nameWithoutExtension != "Block").flatMap { f =>
+      println(f.path)
+      val nodes = JavaToFJ(JavaParser.parse(f.toJava))
+      val fj = f.parent / s"${f.nameWithoutExtension}.fj"
+      fj.write(nodes.mkString("\n\n"))
+      nodes
+    }
+    ProgramM(nodes.toSeq: _*)
   }
-  println(ProgramM(nodes.toSeq:_*))
+
   
 //  val cu = JavaParser.parse(File("src/main/java/redblack/RBNode.java").toJava)
 //  cu.accept(new NodeVisitor with PrintlnVisitor, ())
